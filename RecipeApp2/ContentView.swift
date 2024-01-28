82bb9bf2fa296944dc1dc2e1a395dfe0f6bcf227
joaobug <joaoenfrance@gmail.com>
2024-01-28 20:46:15 -0300

    Initial Commit

---
+++ RecipeApp2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RecipeApp2
+//
+//  Created by Joao Leal on 1/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RecipeApp2/RecipeApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp2App.swift
+//  RecipeApp2
+//
+//  Created by Joao Leal on 1/28/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
